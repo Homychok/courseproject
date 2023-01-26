e7e4b017459c51a30377e1ef643e862d5ce70db9
Alexandra <alexandravolchihina@yandex.ru>
2023-01-26 15:48:33 +0300

    Курсовой проект

--- src/firstandsecondparts/tasks/OneTimeTask.java
+++ src/firstandsecondparts/tasks/OneTimeTask.java
@@ -11,6 +11,6 @@ public class OneTimeTask extends Task {
     }
     @Override
     public boolean appearsIn(LocalDate localDate) {
-       return localDate.isAfter(getDateTime().toLocalDate());
+       return localDate.isEqual(getDateTime().toLocalDate());
     }
 }
